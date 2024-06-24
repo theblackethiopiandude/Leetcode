25ec183dd267bb3d370f3930171f54d46cd87ad6
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-06-24 21:16:19 +0300

    Create 4. Median of Two Sorted Arrays.cpp

---
+++ 4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,25 @@
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>nums;
+        int n1 = nums1.size(), n2 = nums2.size(); 
+        int i = 0, j = 0;
+        for(; i < n1 && j < n2;)
+            if(nums1[i] < nums2[j])
+                nums.push_back(nums1[i++]);
+            else
+                nums.push_back(nums2[j++]);
+        
+        while(i < n1)
+            nums.push_back(nums1[i++]);
+    
+        while(j < n2)
+            nums.push_back(nums2[j++]);
+
+        int d = nums.size() % 2;
+        double med = nums[nums.size() / 2];
+        
+        if(!d)
+            med = (med + nums[(nums.size() / 2) - 1] )/ 2;
+        
+        return med;
+        
+    }
