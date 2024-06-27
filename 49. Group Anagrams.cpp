1d24d37d7210a5dab05acb21e4e217dd831c50f9
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-06-27 21:57:01 +0300

    Create 49. Group Anagrams.cpp

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,14 @@
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> umap;
+        for(auto& word : strs){
+            auto sorted = word;
+            sort(sorted.begin(), sorted.end());
+            umap[sorted].push_back(word);
+        }
+
+        vector<vector<string>> ans;
+        for(auto& pair: umap)
+            ans.push_back(pair.second);
+        
+        return ans;
+    }
