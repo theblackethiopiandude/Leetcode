4d73c4856b888f3006b5c42f2c122251a8daca5f
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-06-30 20:01:24 +0300

    Create 1817. Finding the Users Active Minutes.cpp

---
+++ 1817. Finding the Users Active Minutes.cpp
@@ -0,0 +1,14 @@
+vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        vector<int> ans(k, 0);
+        {
+            unordered_map<int, unordered_set<int>> umap;
+
+            for(auto& log: logs)
+                umap[log[0]].insert(log[1]);
+
+            for(auto& pair: umap)
+                ans[pair.second.size()-1]++;
+        }
+
+        return ans;
+    }
