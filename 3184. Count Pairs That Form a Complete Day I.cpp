2f0126610649bbcf6dfb5595e4fc82d43c609c5f
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-04 09:13:08 +0300

    Update 3184. Count Pairs That Form a Complete Day I.cpp

--- 3184. Count Pairs That Form a Complete Day I.cpp
+++ 3184. Count Pairs That Form a Complete Day I.cpp
@@ -6,3 +6,20 @@ int countCompleteDayPairs(vector<int>& hours) {
                     count++;
         return count;
     }
+
+ int countCompleteDayPairs(vector<int>& hours) {
+        int count = 0;
+        {
+            unordered_map<int, int> umap;
+            for (const int hour : hours){
+                int rem = hour % 24;
+                if(rem){
+                    count += umap[24 - rem];
+                }else{
+                    count += umap[0];
+                }
+                umap[rem]++;
+            }
+        }
+        return count;
+    }
