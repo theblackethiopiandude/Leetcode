afd90c4b64c69fb8f1067a86eda85ee9d95816d0
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-05 19:35:59 +0300

    Update 412. Fizz Buzz.cpp

--- 412. Fizz Buzz.cpp
+++ 412. Fizz Buzz.cpp
@@ -11,3 +11,13 @@ vector<string> fizzBuzz(int n) {
                 ans.push_back(to_string(i));
         return ans;
     }
+
+vector<string> fizzBuzz(int n) {
+        vector<string> ans(n);
+        for(int i = 1; i <= n; i++)
+            ans[i-1] =  (i % 15 == 0) ? "FizzBuzz" :
+                        (i % 5 == 0)  ? "Buzz" :
+                        (i % 3 == 0)  ? "Fizz" :
+                                        to_string(i);
+        return ans;
+    }
