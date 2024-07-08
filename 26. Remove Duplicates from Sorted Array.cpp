38d81c1685bfa4a2fcdad413d5e56c7636890a99
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-08 22:38:45 +0300

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 26. Remove Duplicates from Sorted Array.cpp
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -6,3 +6,20 @@ int removeDuplicates(vector<int>& nums) {
                 it++;
         return nums.size();
     }
+
+
+int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) 
+            return 0;
+    
+        int L = 1, R = 0;
+        while(++R < nums.size())
+            if(nums[R-1] == nums[R])
+                continue;
+            else
+                nums[L++] = nums[R];
+        
+        
+        
+        return L;
+    }
