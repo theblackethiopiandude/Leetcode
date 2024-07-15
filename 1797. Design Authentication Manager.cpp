eb7dcf2980243f2de491681da2298e6843e9a6c1
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-15 20:47:22 +0300

    Update 1797. Design Authentication Manager.cpp

--- 1797. Design Authentication Manager.cpp
+++ 1797. Design Authentication Manager.cpp
@@ -1,75 +1,30 @@
 class AuthenticationManager {
 public:
-    AuthenticationManager(int timeToLive) : ttl(timeToLive){}
+    
+    AuthenticationManager(int timeToLive):ttl(timeToLive) {}
     
     void generate(string tokenId, int currentTime) {
-        auto node = new Node(make_pair(tokenId, currentTime));
-        addNode(node);
-        umap[tokenId] = node;
+        umap[tokenId]= currentTime + ttl;
     }
     
     void renew(string tokenId, int currentTime) {
         if(umap.count(tokenId)){
-            auto startTime = umap[tokenId]->token.second;
-            if(currentTime < startTime + ttl)
-                moveToFront(umap[tokenId], currentTime);
+            if(currentTime < umap[tokenId])
+                umap[tokenId] = currentTime + ttl;
         }
     }
     
     int countUnexpiredTokens(int currentTime) {
-        auto node = head;
         int count = 0;
-        while(node != nullptr){
-            if(currentTime < node->token.second + ttl)
+        for(auto [_, expiryTime]: umap) 
+            if(currentTime < expiryTime)
                 count++;
-            else{
-                if(node->next)
-                    node->next = nullptr;
-                tail = node;
-                break;
-            }
-                
-            
-            node = node->next;
-        }
+
         return count;
     }
     private:
-        class Node{
-            public:
-                pair<string, int> token;
-                Node* next = nullptr;
-                Node* prev = nullptr;
-                explicit Node(const pair<string, int>& token):token(token){}
-        };
-
-        Node* head = nullptr;
-        Node* tail = nullptr;
-        unordered_map<string, Node*> umap;
+        unordered_map<string, int> umap;
         int ttl;
-        
-        void addNode(Node* node){
-            if(head == nullptr){
-                head = node;
-                tail = node;
-            }else{
-                node->next = head;
-                head->prev = node;
-                head = node;
-            }
-        }
-        void moveToFront(Node* node, int currentTime){
-            if(node == head){
-            }else if(node == tail){
-                tail->prev->next = nullptr;
-                tail = tail->prev;
-            }else{
-                node->prev->next = node->next;
-                node->next->prev = node->prev;
-            }
-            node->token.second = currentTime;
-            addNode(node);
-        }
 };
 
 /**
