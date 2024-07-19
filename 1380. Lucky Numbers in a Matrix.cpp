00a31d9788ba9d9b2024a38d08e32df65c2ff801
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-19 04:41:18 +0300

    Update 1380. Lucky Numbers in a Matrix.cpp

--- 1380. Lucky Numbers in a Matrix.cpp
+++ 1380. Lucky Numbers in a Matrix.cpp
@@ -23,3 +23,11 @@ vector<int> luckyNumbers (vector<vector<int>>& matrix) {
 
         return ans;
     }
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
