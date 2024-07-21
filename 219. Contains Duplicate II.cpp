4942ce9d4167130e83bc156bff18aacdc4837745
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-22 01:24:02 +0300

    Update 219. Contains Duplicate II.cpp

--- 219. Contains Duplicate II.cpp
+++ 219. Contains Duplicate II.cpp
@@ -11,3 +11,22 @@ bool containsNearbyDuplicate(vector<int>& nums, int k) {
 
         return false;
     }
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (k == 0)
+            return false;
+
+        {
+            unordered_set<int> s;
+            for(int i = 0; i < nums.size(); i++){
+                if(s.count(nums[i]))
+                    return true;
+                
+                if(s.size() == k)
+                    s.erase(nums[i-k]);
+                
+                s.insert(nums[i]);
+            }
+        }
+        return false;
+    }
