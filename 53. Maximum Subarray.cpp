38afdfb124d00a2c802296d4023226f5e6c67c7d
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-24 19:32:33 +0300

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -2,7 +2,7 @@ int maxSubArray(vector<int>& nums) {
         int maxSum = nums[0];
         int currSum = 0;
         for(int num: nums){
-            currSum = max(currSum, 0) + num;
+            currSum = max(currSum, 0) + num; // currSum = max(currSum + num, num);
             maxSum = max(maxSum, currSum);
         }
 
