91c9c6458416845b171425a11cf59c41f9bece91
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-26 10:36:22 +0300

    Update 1456. Maximum Number of Vowels in a Substring of Given Length.cpp

--- 1456. Maximum Number of Vowels in a Substring of Given Length.cpp
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -23,3 +23,20 @@ int maxVowels(string s, int k) {
         }
         return maxVowel;
     }
+
+
+int maxVowels(string s, int k) {
+        int maxVowel = 0;
+        unordered_set<char> v({'a', 'e', 'i', 'o', 'u'});
+        for(int L = 0, R = 0, vowel = 0; R < s.size(); R++){
+            if(v.count(s[R]))
+                vowel++;
+            
+            if(R - L + 1 == k){
+                maxVowel = max(maxVowel, vowel);
+                if(v.count(s[L++]))
+                    vowel--;
+            }
+        }
+        return maxVowel;
+    }
