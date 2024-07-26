19756e340781684a72cf14c4e8c9dd7a5c4a89fe
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-26 11:01:48 +0300

    Update 2414. Length of the Longest Alphabetical Continuous Substring.cpp

--- 2414. Length of the Longest Alphabetical Continuous Substring.cpp
+++ 2414. Length of the Longest Alphabetical Continuous Substring.cpp
@@ -10,3 +10,13 @@ int longestContinuousSubstring(string s) {
         }
         return maxSeq;
     }
+
+int longestContinuousSubstring(string s) {
+        int maxSeq = 1;
+        for(int R = 1, currSeq = 1; R < s.size(); R++){
+            currSeq = (s[R] - s[R-1] == 1)? currSeq + 1 : 1;    
+            maxSeq = max(maxSeq, currSeq);
+        }
+        
+        return maxSeq;
+    }
