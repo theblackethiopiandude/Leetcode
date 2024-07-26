022f18d61ad9bca82e22036c32bd4d21d28f8e5a
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-26 10:59:16 +0300

    Create 2414. Length of the Longest Alphabetical Continuous Substring.cpp

---
+++ 2414. Length of the Longest Alphabetical Continuous Substring.cpp
@@ -0,0 +1,12 @@
+int longestContinuousSubstring(string s) {
+        int maxSeq = 1;
+        for(int R = 1, currSeq = 1; R < s.size(); R++){
+            if(s[R] - s[R-1] == 1)
+                currSeq++;
+            else
+                currSeq = 1;
+    
+            maxSeq = max(maxSeq, currSeq);
+        }
+        return maxSeq;
+    }
