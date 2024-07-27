ba35edf59ef98edf53a6c2ab4341ac4c88aa9789
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-07-27 13:56:45 +0300

    Update 20. Valid Parentheses.cpp

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -32,3 +32,25 @@ bool isValid(string s) {
         
         return st.empty();
     }
+
+bool isValid(string s) {
+        stack<char> st;
+        {
+            unordered_map<char, char> cmap;
+            unordered_set<char> oset({'[', '(', '{'});
+
+            cmap = {{']', '['}, {')', '('}, {'}', '{'}};
+
+            for(char ch: s){
+                if(oset.count(ch))
+                    st.push(ch);
+                else{
+                    if(st.empty() || cmap[ch] !=  st.top())
+                        return false;
+                    st.pop();
+                }      
+            }
+        }
+
+        return st.empty();
+    }
