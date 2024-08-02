7a8fa74f12ca8dd14b8f3e1527da26e01617a057
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-02 05:26:12 +0300

    Update 496. Next Greater Element I.cpp

--- 496. Next Greater Element I.cpp
+++ 496. Next Greater Element I.cpp
@@ -16,3 +16,27 @@ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         }
         return ans;
     }
+
+
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans(nums1.size());
+        {
+            unordered_map<int, int> umap;
+            stack<int> st;
+
+            for(int L = 0; L < nums1.size(); L++)
+                umap[nums1[L]] = L;
+
+            for(int R = nums2.size() -1; R >= 0; R--){
+                while(!st.empty() && st.top() < nums2[R])
+                    st.pop();
+                
+                if(umap.count(nums2[R]))         
+                    ans[umap[nums2[R]]] = (st.empty())? -1: st.top();
+                    
+                st.push(nums2[R]);
+            }
+
+        }
+        return ans;
+    }
