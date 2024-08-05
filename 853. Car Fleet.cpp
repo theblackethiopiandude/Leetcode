f0147e2a562068375d3ab28ca20cbf02f254c062
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-05 17:16:02 +0300

    Update 853. Car Fleet.cpp

--- 853. Car Fleet.cpp
+++ 853. Car Fleet.cpp
@@ -13,3 +13,23 @@ int carFleet(int target, vector<int>& position, vector<int>& speed) {
         }
         return st.size();
     }
+
+
+
+int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<float> st;
+        {
+            vector<float> time(target+1, -1);
+            for(int i = 0; i < position.size(); i++)
+                time[position[i]] = (target - position[i]) / (float)speed[i];
+            
+            for(int i = target; i >= 0; i--){
+                if(time[i] == -1)
+                    continue;
+                st.push_back(time[i]);
+                if(st.size() > 1 && st.back() <= *(st.end()-2))
+                    st.pop_back();     
+            }
+        }
+        return st.size();
+    }
