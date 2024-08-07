811f5864941da2052b80308938bbf565956d337b
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-07 13:47:52 +0300

    Create 12. Integer to Roman.cpp

---
+++ 12. Integer to Roman.cpp
@@ -0,0 +1,47 @@
+string intToRoman(int num) {
+        string ans = "";
+        {
+            string n = to_string(num);
+            const int size = n.length();
+            unordered_map<int, char> umap = {
+                {1, 'I'},
+                {5, 'V'},
+                {10, 'X'},
+                {50, 'L'},
+                {100, 'C'},
+                {500, 'D'},
+                {1000, 'M'},
+            };
+            unordered_map<int, int> pow = {
+                {1, 1},
+                {2, 10},
+                {3, 100},
+                {4, 1000},
+            };
+            for(int i = 0; i < n.length(); i++){
+                if(n[i] == '0')
+                    continue;
+                else if(n[i] < '5'){
+                    if(n[i] == '4'){
+                        ans += umap[pow[size - i]];
+                        ans += umap[5 * pow[size - i]]; 
+                    }else{
+                        for(int j = '0'; j < n[i]; j++)
+                            ans += umap[pow[size - i]];
+                    }
+                }else if(n[i] > '5'){
+                    if(n[i] == '9'){
+                        ans += umap[pow[size - i]];
+                        ans += umap[10 * pow[size - i]]; 
+                    }else{
+                        ans += umap[5 * pow[size - i]];
+                        for(int j = '5'; j < n[i]; j++)
+                            ans += umap[pow[size - i]];
+                    }
+                }else{
+                    ans += umap[5 * pow[size - i]];
+                }
+            }
+        }
+        return ans;
+    }
