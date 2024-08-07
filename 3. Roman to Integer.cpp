9e59b61293296bd157e094247bd07b95e3a4f9c5
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-08 00:11:23 +0300

    Create 3. Roman to Integer.cpp

---
+++ 3. Roman to Integer.cpp
@@ -0,0 +1,26 @@
+int romanToInt(string s) {
+        int ans = 0;
+        {
+            unordered_map<char, int> umap = {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+
+            ans += umap[s[s.length() - 1]];
+
+            for(int i = s.length() - 2; i >= 0; i--){
+                if(umap[s[i]] < umap[s[i+1]])
+                    ans -= umap[s[i]];
+                else
+                    ans += umap[s[i]];
+            }
+
+        }
+        
+        return ans;
+  }
