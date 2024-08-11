1ee50cb7c294af3fead6cf3944f9820f91d5b208
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-11 15:13:46 +0300

    Update 202. Happy Number.cpp

--- 202. Happy Number.cpp
+++ 202. Happy Number.cpp
@@ -15,3 +15,24 @@ bool isHappy(int n) {
 
         return sum == 1;
     }
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(digitSquareSum(fast));
+        }while(slow != fast);
+
+        return slow == 1;
+    }
+private:
+    int digitSquareSum(int n){
+        int sum = 0;
+        while(n){
+            sum += ((n%10) * (n%10));
+            n /= 10;
+        }
+        return sum;
+    }
+};
