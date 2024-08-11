9d1f488c1d7cffeab46576efb3ca0328655e6767
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-11 14:37:33 +0300

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -7,3 +7,20 @@ int findDuplicate(vector<int>& nums) {
         }
         return -1;
     }
+
+int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        for(int fast = 0; true;){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(fast == slow)
+                break;
+        }
+        for(int slow2 = 0; true;){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if(slow2 == slow)
+                break;
+        }
+        return slow;
+    }
