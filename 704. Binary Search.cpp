24655052eabef481981f1311f3fb5407375324df
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-08-20 21:59:34 +0300

    Create 704. Binary Search.cpp

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,15 @@
+int search(vector<int>& nums, int target) {
+        int L = 0, R = nums.size() - 1;
+
+        while(L <= R){
+            int mid = (L+R)/2;
+            if(nums[mid] < target)
+                L = mid + 1;
+            else if(nums[mid] > target)
+                R = mid - 1;
+            else
+                return mid;
+        }
+
+        return -1;
+    }
