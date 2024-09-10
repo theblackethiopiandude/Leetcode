ca9facdf947516ca22c05d1681f09ea6873b9b1e
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-09-10 22:32:31 +0300

    Update 1672. Richest Customer Wealth.cpp

--- 1672. Richest Customer Wealth.cpp
+++ 1672. Richest Customer Wealth.cpp
@@ -5,3 +5,14 @@ int maximumWealth(vector<vector<int>>& accounts) {
         
         return maxWealth;
   }
+
+int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+        for(int i = 0, sum = 0; i < accounts.size(); i++, sum = 0){
+            for(int j = 0; j < accounts[i].size(); j++)
+                sum += accounts[i][j];
+            if(sum > max)
+                max = sum;
+        }
+        return max;
+    }
