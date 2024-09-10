50f90553c458947918a7f87ac6cd84131393e266
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-09-10 22:31:58 +0300

    Create 1672. Richest Customer Wealth.cpp

---
+++ 1672. Richest Customer Wealth.cpp
@@ -0,0 +1,7 @@
+int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth = 0;
+        for(const auto& account: accounts)
+            maxWealth = max(maxWealth, accumulate(account.begin(), account.end(), 0));
+        
+        return maxWealth;
+  }
