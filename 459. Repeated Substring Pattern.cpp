a11282d90e27b74fea3f3a8ea1f081103ade1988
Eyosiyas <76533841+theblackethiopiandude@users.noreply.github.com>
2024-09-18 19:41:43 +0300

    Create 459. Repeated Substring Pattern.cpp

---
+++ 459. Repeated Substring Pattern.cpp
@@ -0,0 +1,5 @@
+bool repeatedSubstringPattern(string s) {
+        string doubled = s + s;
+        string sub = doubled.substr(1, doubled.size() - 2);
+        return sub.find(s) != string::npos;
+  }
