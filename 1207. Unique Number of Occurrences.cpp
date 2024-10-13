a8f590959320476370acdbef3a7028f30a82c126
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-10-13 21:13:15 +0300

    Update 1207. Unique Number of Occurrences.cpp

--- 1207. Unique Number of Occurrences.cpp
+++ 1207. Unique Number of Occurrences.cpp
@@ -14,3 +14,17 @@ bool uniqueOccurrences(vector<int>& arr) {
 
         return true;
     }
+
+bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> umap;
+
+        for(int num: arr)
+            umap[num]++;
+        
+        unordered_set<int> uset;
+        for(const auto& [_, value] : umap)
+            if(!uset.insert(value).second)
+                return false;
+
+        return true;
+    }
