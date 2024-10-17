41db542ec455619e017a3364383f377c05ede2e2
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-10-17 23:08:53 +0300

    Update 2666. Allow One Function Call.js

--- 2666. Allow One Function Call.js
+++ 2666. Allow One Function Call.js
@@ -18,3 +18,14 @@ var once = function(fn) {
  * onceFn(1,2,3); // 6
  * onceFn(2,3,6); // returns undefined without calling fn
  */
+
+var once = function(fn) {
+    let called = false;
+    return function(...args){
+        if(!called){
+            called = true;
+            return fn(...args);
+        }
+        return undefined;
+    }
+};
