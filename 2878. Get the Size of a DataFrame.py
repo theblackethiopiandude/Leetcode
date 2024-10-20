b8f3640d98ea6988dd3bdb552afd8f01739ffe00
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-10-20 22:34:17 +0300

    Update 2878. Get the Size of a DataFrame.py

--- 2878. Get the Size of a DataFrame.py
+++ 2878. Get the Size of a DataFrame.py
@@ -3,3 +3,7 @@ import pandas as pd
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
     row, column = players.shape
     return [row, column]
+
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
