d9a89728f29cc7d950ccd580b7a605f7362caff1
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-10-20 23:08:50 +0300

    Update 2885. Rename Columns.py

--- 2885. Rename Columns.py
+++ 2885. Rename Columns.py
@@ -3,3 +3,6 @@ import pandas as pd
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
     students.columns = ['student_id', 'first_name', 'last_name', 'age_in_years']
     return students
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns={'id': 'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'})
