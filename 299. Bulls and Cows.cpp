ce9af80483d96acf1f48e145d194233fdfbc4fac
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-11-01 21:22:07 +0300

    Update 299. Bulls and Cows.cpp

--- 299. Bulls and Cows.cpp
+++ 299. Bulls and Cows.cpp
@@ -28,3 +28,30 @@ string getHint(string secret, string guess) {
         return oss.str();
 
     }
+
+string getHint(string secret, string guess) {
+        unordered_map<char, int> umap;
+        int x = 0, y = 0;
+
+        for(int i = 0; i < secret.length(); i++){
+            if(guess[i] == secret[i])
+                x++;
+            else
+                umap[secret[i]]++;
+        }
+        
+        for(int i = 0; i < secret.length(); i++){
+            if(umap.count(guess[i])){
+                if(guess[i] != secret[i] && umap[guess[i]] > 0){
+                    y++;
+                    umap[guess[i]]--;
+                }
+
+            }
+        }
+
+        ostringstream oss;
+        oss << x << 'A' << y << 'B';
+
+        return oss.str();
+    }
