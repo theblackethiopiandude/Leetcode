1684de24a080e43b9c90f1c4d884365ff3c83906
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-11-08 22:56:57 +0300

    Create 169. Majority Element.cpp

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,17 @@
+int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        for(const int num: nums)
+            umap[num]++;
+        
+        pair<int, int> ans(0,0);
+        int maxc = nums.size() / 2;
+        for(const auto& [element, count] : umap)
+            if(count > (nums.size() / 2)){
+                if(count >= maxc){
+                    ans.first = element;
+                    ans.second =  count;
+                }
+            }
+
+        return ans.first;
+    }
