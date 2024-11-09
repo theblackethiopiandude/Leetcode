523b09d498a6bee069f25d4015514d80c7cb5349
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-11-09 21:27:14 +0300

    Update 448. Find All Numbers Disappeared in an Array.cpp

--- 448. Find All Numbers Disappeared in an Array.cpp
+++ 448. Find All Numbers Disappeared in an Array.cpp
@@ -7,3 +7,15 @@ vector<int> findDisappearedNumbers(vector<int>& nums) {
         
         return ans;
     }
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1]);
+        
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] > 0)
+                ans.push_back(i+1);
+        
+        return ans;
+    }
