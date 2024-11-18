76006eb0bc807502aa0b43938d53d9b53cd58481
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-11-18 23:12:17 +0300

    Create 2798. Number of Employees Who Met the Target.kt

---
+++ 2798. Number of Employees Who Met the Target.kt
@@ -0,0 +1,8 @@
+fun numberOfEmployeesWhoMetTarget(hours: IntArray, target: Int): Int {
+        var ans = 0;
+        for(hour in hours)
+            if(hour >= target)
+                ans++;
+                
+        return ans;
+    }
