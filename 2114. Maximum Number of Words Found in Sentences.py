c7f95a77839dd589d1f3c7f90999056665b54373
Eyosiyas <eyosiyashabtemariam@gmail.com>
2024-12-03 20:44:47 +0300

    Create 2114. Maximum Number of Words Found in Sentences.py

---
+++ 2114. Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,6 @@
+def mostWordsFound(self, sentences: List[str]) -> int:
+        ans = 0
+        for sentence in sentences:
+            ans = max(ans, len(sentence.split(" ")))
+        
+        return ans
