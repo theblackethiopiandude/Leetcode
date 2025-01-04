c6b562d09c98cd720006318475a1997eb1976eca
Eyosiyas <eyosiyashabtemariam@gmail.com>
2025-01-04 21:31:20 +0300

    Update 1930. Unique Length-3 Palindromic Subsequences.cpp

--- 1930. Unique Length-3 Palindromic Subsequences.cpp
+++ 1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -40,3 +40,25 @@ int countPalindromicSubsequence(string s) {
 
         return palindromes.size();
     }
+
+int countPalindromicSubsequence(string s) {
+        unordered_set<string> palindromes;
+        unordered_set<char> L;
+        unordered_map<char, int> R;
+
+        for(const char c : s)
+            R[c]++;
+        
+        for(const char m : s){
+            R[m]--;
+            
+            for(const auto letter : L)
+                if(R[letter] > 0)
+                    palindromes.insert(string(1, letter) + m);
+
+
+            L.insert(m);
+        }
+
+        return palindromes.size();
+    }
