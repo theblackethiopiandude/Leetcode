b79730605f8faa77fe61f9a91c8299eaa5fe1bcd
Eyosiyas <eyosiyashabtemariam@gmail.com>
2025-09-13 08:21:28 +0300

    Update 3541. Find Most Frequent Vowel and Consonant.kt

--- 3541. Find Most Frequent Vowel and Consonant.kt
+++ 3541. Find Most Frequent Vowel and Consonant.kt
@@ -16,3 +16,31 @@ class Solution {
         else -> false;
     }
 }
+
+// Beats 100%
+class Solution {
+    fun maxFreqSum(s: String): Int {
+        val freq = IntArray(26)
+
+        for(c in s){
+            freq[c - 'a']++
+        }
+
+        var maxVowelCount = 0
+        var maxConsonantCount = 0
+
+        for(i in freq.indices){
+            if(('a' + i).isVowel())
+                maxVowelCount = maxOf(maxVowelCount, freq[i])
+            else
+                maxConsonantCount = maxOf(maxConsonantCount, freq[i])
+        }
+        
+        return maxVowelCount + maxConsonantCount
+    }
+
+    fun Char.isVowel(): Boolean = when(this){
+        'a', 'e', 'i', 'o', 'u' -> true;
+        else -> false;
+    }
+}
